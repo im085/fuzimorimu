21c36e0caf163cdfce8af2e839dc5623fcd5cd13
Akiko Morimura <mo8ri5@im085ztto.toshima.ne.jp>
2020-01-20 00:10:21 +0900

    0:10時点

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from getpass import getpass #パスワード取得
 #エクセルとの紐付け
 kamoku = openpyxl.load_workbook('./kamoku.xlsx')
 data = kamoku['data']
+data_t = kamoku['data_t']
 
 userdata = openpyxl.load_workbook('./userdata.xlsx')
 userlist = userdata['userlist']
@@ -125,9 +126,7 @@ class user:
 
     #学年の表示
     def grade_show(self, input1):
-        if int(input1['b2'].value) in {1,2,3,4}:
-            return int(input1['b2'].value)
-        return 0
+        return input1['b2'].value
     
     #教職の入力
     def tp(self, input1, inputsave):
@@ -140,29 +139,56 @@ class user:
                     break
                 print('ERROR：もう一度入力し直してください')
         print('COMPLETED: 正常に登録されました')
-        return 0
-                
 
+    #教職の表示
+    def tp_show(self, input1):
+        c = 0
+        for i in range(3, 8):
+            if input1[f'b{i}'].value == 1:
+                print(input1[f'a{i}'].value + " ", end="")
+                c = c + 1
+        if c == 0:
+            print('なし')
 user = user()
 
 class calc:
-    def gpa(self, input3, data):
+    
+    #学科GPA
+    def gpa(self, input2, data):
         sum1 = 0
         sum2 = 0
         for i in range(2,166):
-            sum1 = sum1 + data[f'c{i}'].value * (input3[f'd{i}'].value*4 + input3[f'e{i}'].value*3 + input3[f'f{i}'].value*2 + input3[f'g{i}'].value*1)
-            sum2 = sum2 + data[f'c{i}'].value * (input3[f'd{i}'].value + input3[f'e{i}'].value + input3[f'f{i}'].value + input3[f'g{i}'].value + input3[f'h{i}'].value)
+            sum1 = sum1 + data[f'c{i}'].value * (input2[f'd{i}'].value*4 + input2[f'e{i}'].value*3 + input2[f'f{i}'].value*2 + input2[f'g{i}'].value*1)
+            sum2 = sum2 + data[f'c{i}'].value * (input2[f'd{i}'].value + input2[f'e{i}'].value + input2[f'f{i}'].value + input2[f'g{i}'].value + input2[f'h{i}'].value)
         return float(sum1/sum2)
 
-    def gpaall(self):
-        return 0
+    #GPA
+    def gpaall(self, input2, input3, input4, data, data_t):
+        sum1 = 0
+        sum2 = 0
+        #input2の入力
+        for i in range(2,166):
+            sum1 = sum1 + data[f'c{i}'].value * (input2[f'd{i}'].value*4 + input2[f'e{i}'].value*3 + input2[f'f{i}'].value*2 + input2[f'g{i}'].value*1)
+            sum2 = sum2 + data[f'c{i}'].value * (input2[f'd{i}'].value + input2[f'e{i}'].value + input2[f'f{i}'].value + input2[f'g{i}'].value + input2[f'h{i}'].value)
+        #input3の入力
+        for i in range(2, 23):
+            sum1 = sum1 + data_t[f'c{i}'].value * (input3[f'd{i}'].value*4 + input3[f'e{i}'].value*3 + input3[f'f{i}'].value*2 + input3[f'g{i}'].value*1)
+            sum2 = sum2 + data_t[f'c{i}'].value * (input3[f'd{i}'].value + input3[f'e{i}'].value + input3[f'f{i}'].value + input3[f'g{i}'].value + input3[f'h{i}'].value)
+        #input4の入力
+        i = 2
+        while(input4[f'b{i}'].value != None):
+            sum1 = sum1 + input4[f'b{i}'].value * (input4[f'd{i}'].value*4 + input4[f'e{i}'].value*3 + input4[f'f{i}'].value*2 + input4[f'g{i}'].value*1 + input4[f'i{i}'].value*4)
+            sum2 = sum2 + input4[f'b{i}'].value * (input4[f'd{i}'].value + input4[f'e{i}'].value + input4[f'f{i}'].value + input4[f'g{i}'].value + input4[f'h{i}'].value + input4[f'i{i}'].value + input4[f'j{i}'].value)
+            i = i + 1
+        return float(sum1/sum2)
 
+    #研究室に入れるか
     def labo(self):
         return 0
 
+    #卒業できるか
     def graduate(self):
         return 0
-
 calc = calc()
 
 def reset():
@@ -191,16 +217,20 @@ def main():
                     def inputsave():    #保存用関数（引数として使う）
                         input_.save(f'./data/{logindata[1]}/input.xlsx')
                     print(f'ようこそ {logindata[1]} さん')
-                    #ここにログイン後の操作を記述予定
                     while(True):
                         func.txt('after_login')
                         c2 = int(input())
                         if c2 in {1, 2, 3, 4, 5, 6}:
                             #1: ユーザー情報・成績確認
                             if c2 == 1:
-                                print('ユーザーネーム: '+f'{logindata[1]}')
-                                print('コース: ')
-                                pass
+                                print('ユーザーネーム: ' + f'{logindata[1]}')
+                                print('コース: ' + user.corse_show(input1))
+                                print('学年: ' + user.grade_show(input1))
+                                print('教職:')
+                                user.tp_show(input1)
+                                print('学科科目GPA: ' + calc.gpa(input2, data))
+                                print('GPA: ' + calc.gpaall(input2, input3, input4, data, data_t))
+                                print('卒業研究')
                             #2: アカウント情報変更
                             elif c2 == 2:
                                 pass
@@ -213,7 +243,7 @@ def main():
                             #5: その他の科目入力
                             elif c2 == 5:
                                 pass
-                            #
+                            #6: ログアウト
                             elif c2 == 6:
                                 print('ログアウトしました。')
                                 break
